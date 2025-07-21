b1e02c47825f4dadf3d984902de084901d83d922
MartinBock1 <martinbock@arcor.de>
2025-07-21 14:54:05 +0000

    made app ready for server

--- core/urls.py
+++ core/urls.py
@@ -14,13 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
     SpectacularRedocView,
 )
+from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +33,4 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-]
+] + staticfiles_urlpatterns()
